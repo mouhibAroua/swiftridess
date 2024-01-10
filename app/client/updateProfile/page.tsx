a3c9366923f058716c7f9a5e5aff5fad19bb7982
marwaZbidi <zbidimarwa0@gmail.com>
2024-01-10 13:54:40 +0100

    cloudinary

--- app/client/updateProfile/page.tsx
+++ app/client/updateProfile/page.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import { useState } from "react";
 
 interface Client {
-    FullName: string;
+    id:Number;
+    fullName: string;
     image_user: string;
     phoneNumber: string;
     email: string;
@@ -11,8 +12,13 @@ interface Client {
   }
 
 const UpdateProfile=()=>{
-    const [imgUrl, setImgUrl] = useState<string[]>([]);
-
+    const [imgUrl, setImgUrl] = useState<string>("");
+    const [fullName, setFullName] = useState<string>("")
+    const [phoneNumber, setPhoneNumber] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [newPassword, setNewPassword] = useState<string>("")
+    const [show, setShow] = useState<Boolean>(false)
 
     const addPicture = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -20,13 +26,13 @@ const UpdateProfile=()=>{
         if (file) {
           const formData = new FormData();
           formData.append('file', file);
-          formData.append('upload_preset', 'dfsyqvvim');
+          formData.append('upload_preset', 'pa4ezjqw');
     
           axios
-            .post('http://api.cloudinary.com/v1_1/dfsyqvvim/image/upload', formData)
+            .post('https://res.cloudinary.com/dfsyqvvim/image/upload', formData)
             .then((res) => {
               console.log('secure', res.data.secure_url);
-              setImgUrl([res.data.secure_url]);
+              setImgUrl(res.data.secure_url);
               console.log('url', imgUrl);
             })
             .catch((err) => {
@@ -35,6 +41,18 @@ const UpdateProfile=()=>{
             });
         }
       };
+
+      const modifyProfile = (user:Object) => {
+              axios
+                .put("http://localhost:3000/api/users/${id}", user)
+                .then((res) => {
+          console.log(res.data,"res")
+                  alert("You successfully updated your account");
+                  })
+                .catch((err) =>
+          
+          console.log(err)
+                  )};
     
 
 return(
@@ -48,14 +66,19 @@ return(
                     <div className="flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
 
                         <img className="object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 dark:ring-indigo-500"
-                            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZhY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
-                            alt="Bordered avatar"/>
+                            src=""
+                            alt=""/>
 
                         <div className="flex flex-col space-y-5 sm:ml-8">
                             <button type="button"
-                                className="text-white py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
+                                className="text-white py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
+                                onClick={()=>{setShow(!show)}}>
                                 Change picture
                             </button>
+                            {show &&
+                            <div>
+                            <input type="file" onChange={(e) => addPicture(e)} />
+                          </div>}
                             <button type="button"
                                 className="py-3.5 px-7 text-base font-medium text-indigo-900 focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
                                 Delete picture
@@ -73,7 +96,7 @@ return(
                                    Full Name</label>
                                 <input type="text" id="Full-Name"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Full Name" />
+                                    placeholder="Full Name" onChange={(e)=>setFullName(e.target.value)}/>
                             </div>
 
                             <div className="w-full">
@@ -82,7 +105,7 @@ return(
                                     Phone Number</label>
                                 <input type="text" id="phone"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Phone Number"  />
+                                    placeholder="Phone Number" onChange={(e)=>setPhoneNumber(e.target.value)} />
                             </div>
 
                         </div>
@@ -93,7 +116,7 @@ return(
                                 email</label>
                             <input type="email" id="email"
                                 className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                placeholder="your.email@mail.com" />
+                                placeholder="your.email@mail.com" onChange={(e)=>setEmail(e.target.value)} />
                         </div>
 
                         <div className="mb-2 sm:mb-6">
@@ -101,12 +124,21 @@ return(
                                 className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Password</label>
                             <input type="password" id="password"
                                 className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                placeholder="Password" />
+                                placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                        </div>
+                        <div className="mb-2 sm:mb-6">
+                            <label 
+                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">New Password</label>
+                            <input type="password" id="New-password"
+                                className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                                placeholder="New Password" onChange={(e)=>setNewPassword(e.target.value)} />
                         </div>
 
                         <div className="flex justify-end">
                             <button type="submit"
-                                className="text-black bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
+                                className="text-black bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+                                onClick={()=>{modifyProfile({fullName:fullName,phoneNumber:phoneNumber,email:email,password:password,newPassword:newPassword})}}
+                                >Save</button>
                         </div>
 
                     </div>
