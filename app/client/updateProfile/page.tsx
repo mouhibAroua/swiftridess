5a962664f4d1b5c53c4ec5ddc5acc6b8789666d8
marwaZbidi <zbidimarwa0@gmail.com>
2024-01-10 11:44:00 +0100

    done

--- app/client/updateProfile/page.tsx
+++ app/client/updateProfile/page.tsx
@@ -1,15 +1,49 @@
+"use client"
+import axios from "axios";
+import { useState } from "react";
 
+interface Client {
+    FullName: string;
+    image_user: string;
+    phoneNumber: string;
+    email: string;
+    password: string
+  }
 
 const UpdateProfile=()=>{
+    const [imgUrl, setImgUrl] = useState<string[]>([]);
+
+
+    const addPicture = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+    
+        if (file) {
+          const formData = new FormData();
+          formData.append('file', file);
+          formData.append('upload_preset', 'dfsyqvvim');
+    
+          axios
+            .post('http://api.cloudinary.com/v1_1/dfsyqvvim/image/upload', formData)
+            .then((res) => {
+              console.log('secure', res.data.secure_url);
+              setImgUrl([res.data.secure_url]);
+              console.log('url', imgUrl);
+            })
+            .catch((err) => {
+              console.log(formData);
+              console.log(err);
+            });
+        }
+      };
+    
+
 return(
     
 
-<div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931] ml-[300px]">
+<div className="max-h-screen justify-center items-center bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
     <main className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
         <div className="p-2 md:p-4">
             <div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-                <h2 className="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
-
                 <div className="grid max-w-2xl mx-auto mt-8">
                     <div className="flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
 
@@ -35,20 +69,20 @@ return(
                             className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
                             <div className="w-full">
                                 <label 
-                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Your
-                                    first name</label>
-                                <input type="text" id="first_name"
+                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">
+                                   Full Name</label>
+                                <input type="text" id="Full-Name"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Your first name" value="Jane" />
+                                    placeholder="Full Name" />
                             </div>
 
                             <div className="w-full">
                                 <label 
-                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Your
-                                    last name</label>
-                                <input type="text" id="last_name"
+                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">
+                                    Phone Number</label>
+                                <input type="text" id="phone"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Your last name" value="Ferguson" />
+                                    placeholder="Phone Number"  />
                             </div>
 
                         </div>
@@ -64,23 +98,15 @@ return(
 
                         <div className="mb-2 sm:mb-6">
                             <label 
-                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Profession</label>
-                            <input type="text" id="profession"
+                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Password</label>
+                            <input type="password" id="password"
                                 className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                placeholder="your profession" />
-                        </div>
-
-                        <div className="mb-6">
-                            <label 
-                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Bio</label>
-                            <textarea id="message" 
-                                className="block p-2.5 w-full text-sm text-indigo-900 bg-indigo-50 rounded-lg border border-indigo-300 focus:ring-indigo-500 focus:border-indigo-500 "
-                                placeholder="Write your bio here..."></textarea>
+                                placeholder="Password" />
                         </div>
 
                         <div className="flex justify-end">
                             <button type="submit"
-                                className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
+                                className="text-black bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
                         </div>
 
                     </div>
