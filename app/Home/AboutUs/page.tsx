e67ce3f911ab51392962fdf659cac844d3505d40
Mohamed Amine Merdessi <hamamerdessi00@gmail.com>
2024-01-17 09:52:34 +0100

    updates/fixes/

--- app/Home/AboutUs/page.tsx
+++ app/Home/AboutUs/page.tsx
@@ -1,18 +1,20 @@
 const AboutUs = () => {
-    return ( 
-        <div className="mt-[100px] ">
-            <h1  className="flex justify-center font-semibold text-3xl">About us</h1>
-            <hr className="border-t-4 border-oronge-500 mb-4 w-[180px] ml-[650px]"/> 
-        <div className="flex  ml-[70px] mt-[100px] space-x-40">
-            <div className="max-w-[600px]">
-                <img src="https://cdn.discordapp.com/attachments/1113064451806076990/1195313008692449290/Rectangle_5_1.jpg?ex=65b388fd&is=65a113fd&hm=16673a7a4a50b2b55bec38e1f3d79256f62f2ebd9620da525d7b0ec346a7d028&" alt="" />
+    return (
+        <div className="mt-[100px]">
+            <h1 className="flex justify-center font-semibold text-3xl">Unmatched excellence and customer satisfaction</h1>
+
+            <div className="w-[900px] ml-60 mt-20">
+                <img src="https://images2.imgbox.com/7f/62/9xR78TOz_o.png" alt="" />
+                <p className="text-center mt-4 text-2xl">Our Dedication to providing exceptional services set us apart from the competition. From the Moment you engage with us , we strive to exceed your expectations in every interaction</p>
+
             </div>
-            <div className="flex text-center  font-medium italic">
-                <span>hello</span>
+
+            <div className="flex ml-[70px] mt-[100px] space-x-40">
+                <div className="flex text-center font-medium italic">
+                </div>
             </div>
         </div>
-        </div>
-     );
+    );
 }
- 
+
 export default AboutUs;
