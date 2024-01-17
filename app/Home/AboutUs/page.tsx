c1e69e444d3fa97beb358315b235732564bc0f50
houssemboulaares <houssem.boulaares.123@gmail.com>
2024-01-17 09:50:56 +0100

    done

--- app/Home/AboutUs/page.tsx
+++ app/Home/AboutUs/page.tsx
@@ -1,45 +1,61 @@
 "use client"
-import React, { useEffect, useState,useRef } from 'react';
-import { motion } from "framer-motion";
-import fadeIn from './fadeIn'
+// AboutUs.tsx
+import React, { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import fadeIn from './fadeIn';
 
 const AboutUs = () => {
-    const [animationTriggered, setAnimationTriggered] = useState<boolean>(false);
-    const scrollDown = useRef<HTMLDivElement>(null);
+  const [animationTriggered, setAnimationTriggered] = useState<boolean>(false);
+  const scrollDown = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-          if (scrollDown.current) {
-            const down = scrollDown.current.getBoundingClientRect();
-            const vs = down.top < window.innerHeight
-            setAnimationTriggered(vs);
-          }
-        };
-        window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollDown.current) {
+        const down = scrollDown.current.getBoundingClientRect();
+        const vs = down.top < window.innerHeight;
+        setAnimationTriggered(vs);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
 
-      }, []);
-  
-    return ( 
-        <motion.div 
-         ref={scrollDown}
-        variants={fadeIn('up', 0.4)}
-         initial='hidden'
-        animate={animationTriggered ? 'show' : 'hidden'}
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-        
-        className="mt-[100px] ">
-            <h1  className="flex justify-center font-semibold text-3xl">About us</h1>
-            <hr className="border-t-4 border-oronge-500 mb-4 w-[180px] ml-[650px]"/> 
-        <div className="flex  ml-[130px] mt-[100px] space-x-40">
-            <div className="max-w-[600px]">
-                <img src="https://cdn.discordapp.com/attachments/1113064451806076990/1195313008692449290/Rectangle_5_1.jpg?ex=65b388fd&is=65a113fd&hm=16673a7a4a50b2b55bec38e1f3d79256f62f2ebd9620da525d7b0ec346a7d028&" alt="" />
-            </div>
-            <div className="flex text-center  font-medium italic">
-                <span>hello</span>
-            </div>
-        </div>
-        </motion.div>
-     );
-}
- 
+  return (
+    <motion.div
+      className="mt-[100px]"
+      ref={scrollDown}
+      variants={fadeIn('up', 0.4)}
+      initial="hidden"
+      animate={animationTriggered ? 'show' : 'hidden'}
+    >
+      <h1 className="flex justify-center font-semibold text-3xl">
+        Unmatched excellence and customer satisfaction
+      </h1>
+
+      <motion.div
+        className="w-[900px] ml-60 mt-20"
+        variants={fadeIn('down', 0.6)} 
+      >
+        <motion.img
+          src="https://images2.imgbox.com/7f/62/9xR78TOz_o.png"
+          alt=""
+          variants={fadeIn('left', 0.6)} 
+        />
+        <p className="text-center mt-4 text-2xl">
+          Our dedication to providing exceptional services sets us apart from
+          the competition. From the moment you engage with us, we strive to
+          exceed your expectations in every interaction.
+        </p>
+      </motion.div>
+
+      <div className="flex ml-[70px] mt-[100px] space-x-40">
+        <div className="flex text-center font-medium italic"></div>
+      </div>
+    </motion.div>
+  );
+};
+
 export default AboutUs;
--- app/Home/SearchBuyOccasion/page.tsx
+++ app/Home/SearchBuyOccasion/page.tsx
@@ -1,51 +1,71 @@
 "use client"
-import React, { useEffect, useState,useRef } from 'react';
-import { motion } from "framer-motion";
+import React, { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 import fadeIn from '../AboutUs/fadeIn';
 
 const Occasion = () => {
-    const [animationTriggered, setAnimationTriggered] = useState<boolean>(false);
-    const scrollDown = useRef<HTMLDivElement>(null);
+  const [animationTriggered, setAnimationTriggered] = useState<boolean>(false);
+  const scrollDown = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-          if (scrollDown.current) {
-            const down = scrollDown.current.getBoundingClientRect();
-            const vs = down.top < window.innerHeight
-            setAnimationTriggered(vs);
-          }
-        };
-        window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollDown.current) {
+        const down = scrollDown.current.getBoundingClientRect();
+        const vs = down.top < window.innerHeight;
+        setAnimationTriggered(vs);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
 
-      }, []);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    return (
-        <motion.div 
-        ref={scrollDown}
-        variants={fadeIn('up', 0.4)}
-         initial='hidden'
-        animate={animationTriggered ? 'show' : 'hidden'}
-        
-        className="mt-[100px]">
-            <h1 className="flex justify-center font-semibold text-3xl">Occasion</h1>
-            <hr className="border-t-4 border-oronge-500 mb-4 w-[180px] ml-[650px]"/> 
-        <div className="flex justify-center space-x-40 mt-[100px]" >
-            
-            <div className="w-[200px] text-center mb-4 ">
-                <img src="https://cdn.discordapp.com/attachments/1191774717003182175/1195382536692568194/9d586d2acbd77481e4650fe48aea7e0a-removebg-preview.png?ex=65b3c9bd&is=65a154bd&hm=d2c2817137a72b1fe812a597db6555165bc78a4e4a8ff2c4e9a7111f64e81fc5&" alt="" />
-                <h1 className="font-semibold">Weddings</h1>
-            </div>
-            <div className="w-60 text-center mb-4">
-                <img src="https://cdn.discordapp.com/attachments/1113064451806076990/1195294213252857927/43801ed6-940a-4810-aef1-fc7a93783b72_1.png?ex=65b3777b&is=65a1027b&hm=99ead9082d51a60062ef7869e5007b5c509fcd5b97c5c831321bcff53a92d324&" alt="" />
-                <h1 className="font-semibold">Daily Use</h1>
-            </div>
-            <div className="w-60 text-center mb-4">
-                <img src="https://www.petitforestier.com/files/images/file/9b9bab13-765e-4620-ac40-b97696266bdc/iveco-daily-1.png" alt="" />
-                <h1 className="font-semibold">Transporter</h1>
-            </div>
-        </div>
+  return (
+    <motion.div
+      ref={scrollDown}
+      variants={fadeIn('up', 0.4)}
+      initial="hidden"
+      animate={animationTriggered ? 'show' : 'hidden'}
+      className="mt-[100px]"
+    >
+      <h1 className="flex justify-center font-semibold text-3xl">
+        Occasion
+      </h1>
+            <hr className="border-t-4 border-oronge-500 mb-4 w-[180px] ml-[650px]" />
+
+
+      <div className="flex justify-center space-x-40 mt-[100px]">
+        <motion.div
+          className="w-[200px] text-center mb-4"
+          variants={fadeIn('left', 0.6)}
+        >
+          <img
+            src="https://cdn.discordapp.com/attachments/1191774717003182175/1195382536692568194/9d586d2acbd77481e4650fe48aea7e0a-removebg-preview.png?ex=65b3c9bd&is=65a154bd&hm=d2c2817137a72b1fe812a597db6555165bc78a4e4a8ff2c4e9a7111f64e81fc5&"/>
+          <h1 className="font-semibold">Weddings</h1>
         </motion.div>
-    );
-}
+
+        
+        <motion.div
+          className="w-60 text-center mb-4"
+          variants={fadeIn('left', 0.7)}
+        >
+          <img
+            src="https://cdn.discordapp.com/attachments/1113064451806076990/1195294213252857927/43801ed6-940a-4810-aef1-fc7a93783b72_1.png?ex=65b3777b&is=65a1027b&hm=99ead9082d51a60062ef7869e5007b5c509fcd5b97c5c831321bcff53a92d324&"/>
+          <h1 className="font-semibold">Daily Use</h1>
+        </motion.div>
+        <motion.div
+          className="w-60 text-center mb-4"
+          variants={fadeIn('left', 0.8)}
+        >
+          <img
+            src="https://www.petitforestier.com/files/images/file/9b9bab13-765e-4620-ac40-b97696266bdc/iveco-daily-1.png"/>
+          <h1 className="font-semibold">Transporter</h1>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Occasion;
