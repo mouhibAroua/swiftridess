d680c6605193e1198d48a427799d39f2359c98c5
marwaZbidi <zbidimarwa0@gmail.com>
2024-01-18 08:54:53 +0100

    add car design

--- app/company/addCar/page.tsx
+++ app/company/addCar/page.tsx
@@ -96,76 +96,54 @@ const addCar=()=>{
 
     return(
       
-       <div className="grid grid-cols-2 gap-1">
-        
+       <>
+       
         <SideNav/>
-        <div>
-        <h1 className="text-center font-bold-5xl text-5xl mt-[100px] ml-80 ">Add New Car</h1>
-        <div className=" space-y-5 sm:flex-row sm:space-y-0 gap-6">
-        <button className="ml-[500px] mt-[50px] text-white py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
-        onClick={handleButtonClick}>
-            Add Pictures</button>
-         <input 
-            ref={fileInputRef}
-            type="file" 
-            className="hidden"
-            onChange={(e) =>{ addPicture(e) }} 
-          /> 
-        <div className="flex flex-wrap space-x-4 space-y-4">
+      
         
-        {image.slice(0, 5).map((image, index) => (
-        <img 
-        className=" rounded-md border object-cover w-[200px] h-[200px] p-1 ml-[250px]  "
-                            key={index}
-                            src={image}
-                            alt={`previewImage ${index}`}/>
-                            ))}
-                            </div>
-        
-          
-         
-        </div>
-        </div>
-        <div className="mt-[150px]">
-            <div className="grid gap-y-4 grid-cols-2 ">
+        <div className=" bg-gray-800 mb-[-50px]">
+       <h1 className="text-center font-bold-5xl text-5xl  ml-80 text-white ">Add New Vehicle</h1>
+        <div className="grid grid-cols-2">
+        <div className="flex flex-col ml-[300px] mt-[50px] ">
+            
              
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Price/Day :</label>
-  <div className="mt-2">
+ 
     <input
     onChange={(e)=>setPrice(e.target.valueAsNumber)}
       type="number"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
-  </div>
+  
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Brand :</label>
-  <div className="mt-2">
+  
     <input
     onChange={(e)=>setBrand(e.target.value)}
       type="text"
       name="inputname"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
  
-  </div>
+  
   
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Model :</label>
-  <div className="mt-2">
+  
     <input
     onChange={(e)=>setModel(e.target.value)}
       type="text"
       name="inputname"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
  
-  </div>
+ 
   
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Type :</label>
-  <div >
-  <select className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800" 
+ 
+  <select className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]" 
   id="type" value={type}
           onChange={e => setType(e.target.value)}>
           <option value="SUV">SUV</option>
@@ -174,22 +152,22 @@ const addCar=()=>{
           <option value="Sport">Sport</option>
           <option value="Limosine">Limosine</option>
        </select>
-  </div>
+ 
  
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Registration :</label>
-  <div className="mt-2">
+  
     <input
     onChange={(e)=>setRegistration(e.target.value)}
       type="text"
       name="inputname"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
-  </div>
+  
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Fuel :</label>
-  <div className="mt-2">
-  <select className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800" 
+  
+  <select className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]" 
   id="fuel" value={fuelType}
           onChange={e => setFuelType(e.target.value)}>
           <option value="Diesel">Diesel</option>
@@ -197,47 +175,47 @@ const addCar=()=>{
           <option value="Electrique">Electrique</option>
        </select>
    
-    </div>
+    
  
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Mileage :</label>
-  <div className="mt-2">
+  
     <input
     onChange={(e)=>setMileage(e.target.value)}
       type="text"
       name="inputname"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
   
-  </div>
+  
   
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Transmission :</label>
-  <div className="mt-2">
+ 
     <input
     onChange={(e)=>setTransmission(e.target.value)}
       type="text"
       name="inputname"
-      className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+      className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]"
     />
  
-  </div>
+  
   
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Occassion :</label>
-  <div className="mt-2">
-  <select className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800" 
+  
+  <select className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]" 
   id="occasion" value={occasion}
           onChange={e => setOccassion(e.target.value)}>
           <option value="Daily use">Daily Use</option>
           <option value="Marriage">Marriage</option>
           <option value="Transporter">Transporter</option>
        </select>
-  </div>
+ 
   <label  className="ml-[200px] block text-gray-800 font-bold text-lg"
     >Passenger :</label>
-  <div className="mt-2">
-  <select className="block w-56 rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800" 
+  
+  <select className="block w-[400px] rounded-md py-1.5 px-2 ring-1 ring-inset ring-gray-400 focus:text-gray-800 ml-[100px]" 
   id="passenger" value={passengers}
           onChange={e => setPassengers(e.target.value)}>
           <option value="2">2</option>
@@ -245,14 +223,36 @@ const addCar=()=>{
           <option value="5">5</option>
        </select>
   
-  </div>
-  </div>
-  <button className="text-white py-3.5 px-7 text-base font-large text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 mt-10 ml-[240px] w-[300px]"
+  
+  
+  <button className="text-white py-3.5 px-7 text-base font-large text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 mb-0 mt-10 ml-[150px] w-[300px]"
   onClick={()=>{addNewCar()}}>
     add car</button>
   </div>
-
-  </div>
+  <div className=" space-y-5 sm:flex-row sm:space-y-0 gap-6">
+        <button className="ml-[300px] mt-[50px] text-white py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
+        onClick={handleButtonClick}>
+            Add Pictures</button>
+         <input 
+            ref={fileInputRef}
+            type="file" 
+            className="hidden"
+            onChange={(e) =>{ addPicture(e) }} 
+          /> 
+        <div className="flex flex-wrap space-x-4 space-y-4 ml-[200px]">
+        
+        {image.slice(0, 5).map((image, index) => (
+        <img 
+        className=" rounded-md border object-cover w-[200px] h-[200px] p-1  "
+                            key={index}
+                            src={image}
+                            alt={`previewImage ${index}`}/>
+                            ))}
+                            </div>
+        </div>
+        </div>
+        </div>
+  </>
 
         
     )
