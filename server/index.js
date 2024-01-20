ac7c27fc0de24c77067e60e51fb0a30633de3e62
mouhibAroua <mouhibaroua3@gmail.com>
2024-01-20 13:20:01 +0100

    done

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const userHasCars = require('./models/user_carss')
 const Cart =require ('./models/cart.js')
 const product = require ('./models/product.js')
 const userRoutes=require('./routes/usersRoute.js')
+const chatRouter=require('./routes/chatRoute.js')
 const companyRoutes=require('./routes/companyRoute.js')
 const loginRoutes=require ('./routes/loginRoute.js')
 const signupRoutes=require('./routes/singupRoute.js')
@@ -36,6 +37,7 @@ app.use('/api',signupRoutes)
 
 
 app.use('/api',userRoutes)
+app.use('/api',chatRouter)
 app.use('/api',companyRoutes)
 
 app.listen(PORT, () => {
